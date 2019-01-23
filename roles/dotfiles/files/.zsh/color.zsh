c2f11223ca5da698a1f0cf36da77750dcd2c3bbc
Tempor <mac@K94.com>
2019-01-23 04:40:55 +0300

    Total change zsh

---
+++ roles/dotfiles/files/.zsh/color.zsh
@@ -0,0 +1,44 @@
+# Colors.zsh -------------------------------------------------------------------
+# LastChange: Redesign
+# ------------------------------------------------------------------------------
+
+# Shell/Vim --------------------------------------------------------------------
+alias d1='echo "minimalist" > ~/.vim/.theme; color default-dark'
+
+alias d2='echo "hybrid_reverse" > ~/.vim/.theme ; color tomorrow-night'
+
+alias d3='echo "happy_hacking" > ~/.vim/.theme; color gruvbox-dark-medium'
+
+alias d4='echo "dracula" > ~/.vim/.theme; color dracula'
+
+alias d4='echo "two-firewatch" > ~/.vim/.theme; color nord'
+
+alias d5='echo "onedark" > ~/.vim/.theme; color onedark'
+
+alias d6='echo "apprentice" > ~/.vim/.theme; color eighties'
+
+### d7 sierra -- garyish
+
+### d8 snow -- gray
+
+alias l1='echo "github" > ~/.vim/.theme ; color default-light'
+
+alias l2='echo "carbonized-light" > ~/.vim/.theme; color atelier-estuary-light'
+
+
+
+
+# Mark/ VimR -------------------------------------------------------------------
+# ;rm /Applications/VimR.app/Contents/Resources/markdown/github-markdown.css;cp \
+  # /Applications/VimR.app/Contents/Resources/markdown/color/*.css \
+  # /Applications/VimR.app/Contents/Resources/markdown/github-markdown.css
+
+
+## TODO: --------------------------------------------------
+
+# 1. deus needs a new base16 color :
+# 2. ayu needs a new base16 color :
+# 3. deep-space needs a new base16 color:
+# 4. alduin needs a new base16 color
+# 5. ocean-dark needs a custom vim color
+# 6. mexico-light needs a custom vim color
